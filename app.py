01b37017e054d3f64bdd11954b618726e8f0f0bc
Philipp Klaus <philipp.klaus@gmail.com>
2013-03-27 21:57:24 +0100

    Hardening the user input validation.

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ import time
 import string
 from random import choice
 from datadump import pp
-from data import unis, exkursionen, essen, tshirts
+from data import unis, exkursionen, essen, tshirts, exkursionen_dict, essen_dict, tshirts_dict, unis_dict
+import re
 
 d = filedict.FileDict(filename="data/anmeldungen.dict.sqlite")
 
@@ -29,33 +30,90 @@ def signup():
 
 @get('/anmelden')
 def signup():
-    return template('anmelden', unis=unis, exkursionen=exkursionen, essen=essen, tshirts=tshirts)
+    return template('anmelden', unis=unis, exkursionen=exkursionen, essen=essen, tshirts=tshirts, error=None)
+
+class ValidationError(NameError):
+    pass
 
 @post('/anmelden')
 def login_submit():
     reg = dict() # we store the registrant's details in a dictionary
+    error = None
+    # parse and validate user input
+    reg['notes'] = u""
+    reg['tshirts'] = u""
+    reg['food'] = u""
+    reg['exkursion1'], reg['exkursion2'], reg['exkursion3'] = (u"", u"", u"")
+    reg['arbeitskreise'] = u""
+    reg['university_alt'] = u""
+    reg['university'] = u""
+    reg['email'] = u""
+    reg['last_name'] = u""
+    reg['first_name'] = u""
+    try:
+        reg['notes'] = request.forms.get('notes').decode('utf-8').strip()
+    except:
+        error = ValidationError(u'Fehler mit dem Feld Sonstige Wünsche.')
+    try:
+        reg['tshirt'] = request.forms.get('tshirt').decode('utf-8').strip()
+        tshirts_dict[reg['tshirt']]
+    except:
+        error = ValidationError("Bitte T-Shirt korrekt angeben.")
+    try:
+        reg['food'] = request.forms.get('food').decode('ascii').strip()
+    except:
+        error = ValidationError(u'Bitte eine Wahl für das Essen treffen.')
+    try:
+        reg['exkursion3'] = request.forms.get('exkursion3').decode('utf-8').strip()
+        reg['exkursion2'] = request.forms.get('exkursion2').decode('utf-8').strip()
+        reg['exkursion1'] = request.forms.get('exkursion1').decode('utf-8').strip()
+        exkursionen_dict[reg['exkursion1']] and exkursionen_dict[reg['exkursion2']] and exkursionen_dict[reg['exkursion3']]
+    except:
+        error = ValidationError(u"Exkursionen nicht ordentlich angegeben.")
+    try:
+        reg['arbeitskreise'] = request.forms.get('arbeitskreise').decode('utf-8').strip()
+    except:
+        raise ValidationError(u'Arbeitskreise nicht ordentlich angegeben.')
+    try:
+        reg['university_alt'] = request.forms.get('university_alt').decode('utf-8').strip()
+    except:
+        raise ValidationError(u'Alternative Universität nicht ordentlich angegeben.')
+    try:
+        reg['university'] = request.forms.get('university').decode('utf-8').strip()
+    except:
+        raise ValidationError(u'Universität nicht ordentlich angegeben.')
+    if not (reg['university'] == u'n-i-l' or reg['university'] == u'b-w') and reg['university_alt']:
+        error = ValidationError(u'Bitte entweder Universität aus Liste auswählen oder sie selbst in das Textfeld eingeben') 
+    try:
+        reg['email'] = request.forms.get('email_addr').decode('utf-8').strip()
+        if re.match(u'[a-zA-Z0-9\.\-\_]+[@][a-z-]+\.[a-z]+', reg['email']) == None:
+            raise
+    except:
+        error = ValidationError(u"Bitte gültige E-Mail Adresse angeben.")
+    try:
+        reg['last_name'] = request.forms.get('last_name').decode('utf-8').strip().title()
+        if reg['last_name'] == u'': raise
+    except:
+        raise ValidationError(u'Bitte Nachnamen angeben')
+    try:
+        reg['first_name'] = request.forms.get('first_name').decode('utf-8').strip().title()
+        if reg['first_name'] == u'': raise
+    except:
+        error = ValidationError(u'Bitte Vornamen angeben')
+    if error:
+        return template('anmelden', unis=unis, exkursionen=exkursionen, essen=essen, tshirts=tshirts, registrant=reg, error=error.message)
+    # set up less critical parameters
+    reg['id'] = create_id()
     reg['time'] = unixtime()
     reg['ip'] = request.remote_route[0]
-    reg['first_name'] = request.forms.get('first_name').decode('utf-8').title()
-    reg['last_name'] = request.forms.get('last_name').decode('utf-8').title()
-    reg['email'] = request.forms.get('email_addr').decode('utf-8')
-    reg['university'] = request.forms.get('university').decode('utf-8')
-    reg['university_alt'] = request.forms.get('university_alt').decode('utf-8')
-    reg['food'] = request.forms.get('food').decode('ascii')
-    reg['arbeitskreise'] = request.forms.get('arbeitskreise').decode('utf-8')
-    reg['exkursion1'] = request.forms.get('exkursion1').decode('utf-8')
-    reg['exkursion2'] = request.forms.get('exkursion2').decode('utf-8')
-    reg['exkursion3'] = request.forms.get('exkursion3').decode('utf-8')
-    reg['tshirt'] = request.forms.get('tshirt').decode('utf-8')
-    reg['notes'] = request.forms.get('notes').decode('utf-8')
-    if check_registrant(reg):
-        reg['id'] = create_id()
-        reg['confirmed'] = False
-        d[reg['id']] = reg
+    reg['confirmed'] = False
+    # store the registrant in the SQlite based dictionary
+    try:
         confirmation_mail(reg)
-        redirect('/anmeldung/erfolgreich')
-    else:
-        return template('warning', message_title="Fehler", message="Ein Fehler ist aufgetreten.")
+    except:
+        return template('anmelden', unis=unis, exkursionen=exkursionen, essen=essen, tshirts=tshirts, registrant=reg, error=u"Konnte Bestätigungs-E-Mail nicht versenden. Stimmt deine E-Mail Adresse?")
+    d[reg['id']] = reg
+    redirect('/anmeldung/erfolgreich')
 
 @get('/anmeldung/erfolgreich')
 def success():
--- views/anmelden.tpl
+++ views/anmelden.tpl
@@ -1,55 +1,62 @@
 <div id="anmeldeformular">
+%if error:
+<h4> <span class="label label-important">! Fehler mit der Anmeldung</span> </h4>
+
+<div class="alert alert-error">
+{{error}}
+</div>
+%end
 <h4>Anmeldeformular</h4>
 
 <form method="POST">
 
   <label>Vorname:</label>
-  <input type="text" id="first_name" name="first_name" placeholder="Ruppert" required>
+  <input type="text" id="first_name" name="first_name" value="{{registrant['first_name'] if error else ""}}" placeholder="Ruppert" required>
 
   <label>Nachname:</label>
-  <input type="text" id="last_name" name="last_name" placeholder="Weinrich" required>
+  <input type="text" id="last_name" name="last_name" value="{{registrant['last_name'] if error else ""}}" placeholder="Weinrich" required>
 
   <label>E-Mail Adresse:</label>
-  <input type="email" id="email_addr" name="email_addr" required title="Bitte gültige E-Mail Adresse eingeben.">
+  <input type="email" id="email_addr" name="email_addr" value="{{registrant['email'] if error else ""}}" required title="Bitte gültige E-Mail Adresse eingeben.">
 
   <label>Universität:</label>
   <select id="university" name="university">
     %for uni in unis:
-    <option value="{{uni.acronym}}">{{uni.name}}</option>
+    <option value="{{uni.acronym}}" {{"selected" if error and registrant['university'] == uni.acronym else ""}}>{{uni.name}}</option>
     %end
   </select>
 
   <label>Universität (falls nicht in Liste):</label>
-  <input type="text" id="university_alt" name="university_alt">
+  <input type="text" id="university_alt" name="university_alt" value="{{registrant['university_alt'] if error else ""}}">
 
   <label>Arbeitskreis Wünsche:</label>
-  <input type="text" id="arbeitskreise" name="arbeitskreise">
+  <input type="text" id="arbeitskreise" name="arbeitskreise" value="{{registrant['arbeitskreise'] if error else ""}}">
 
   <label>Exkursionswunsch 1:</label>
   <select id="exkursion1" name="exkursion1">
     %for exkursion in exkursionen:
-    <option value="{{exkursion.code}}">{{exkursion.title}}</option>
+    <option value="{{exkursion.code}}" {{"selected" if error and registrant['exkursion1'] == exkursion.code else ""}}>{{exkursion.title}}</option>
     %end
   </select>
 
   <label>Exkursionswunsch 2:</label>
   <select id="exkursion2" name="exkursion2">
     %for exkursion in exkursionen:
-    <option value="{{exkursion.code}}">{{exkursion.title}}</option>
+    <option value="{{exkursion.code}}" {{"selected" if error and registrant['exkursion2'] == exkursion.code else ""}}>{{exkursion.title}}</option>
     %end
   </select>
 
   <label>Exkursionswunsch 3:</label>
   <select id="exkursion3" name="exkursion3">
     %for exkursion in exkursionen:
-    <option value="{{exkursion.code}}">{{exkursion.title}}</option>
+    <option value="{{exkursion.code}}" {{"selected" if error and registrant['exkursion3'] == exkursion.code else ""}}>{{exkursion.title}}</option>
     %end
   </select>
 
   <label>Ernährung:</label>
   %for choice in essen:
     <label class="radio">
-      <input type="radio" name="food" id="{{choice.code}}" value="{{choice.code}}" {{'checked' if choice.code == u'fleisch' else ''}}>
+      <input type="radio" name="food" id="{{choice.code}}" value="{{choice.code}}" {{'checked' if error and registrant['food'] == choice.code else ('checked' if not error and choice.code == u'fleisch' else '')}}>
       {{choice.commitment}}
     </label>
   %end
@@ -58,12 +65,12 @@
   <label>T-Shirt Größe:</label>
   <select id="tshirt" name="tshirt">
     %for tshirt in tshirts:
-        <option value="{{tshirt.code}}" {{'selected' if tshirt.code == u'M-m' else ''}}>{{tshirt.name}}</option>
+        <option value="{{tshirt.code}}" {{'selected' if error and registrant['tshirt'] == tshirt.code else ('selected' if not error and tshirt.code == u'L-m' else '')}}>{{tshirt.name}}</option>
     %end
   </select>
 
   <label>Sonstige Wünsche:</label>
-  <textarea rows="3" id="notes" name="notes"></textarea>
+<textarea rows="3" id="notes" name="notes">{{registrant['notes'] if error else ""}}</textarea>
 
   <button type="submit" class="btn btn-large btn-success">Für ZaPF anmelden</button>
 </form>
